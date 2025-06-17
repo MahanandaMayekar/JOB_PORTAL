ae9d6eae740ea8142e097b7bfc886e5d84bb2fec
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-06-17 11:49:09 +0530

    added employer schema

---
+++ backend/src/employer/employer.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { EmployerService } from './employer.service';
+import { EmployerController } from './employer.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Employer, EmployerSchema } from './schema/employer.schema';
+import { Job, JobsSchema } from 'src/jobs/schrma/jobs.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([
+    { name: Employer.name, schema: EmployerSchema },
+    { name: Job.name, schema: JobsSchema }
+  ])],
+  providers: [EmployerService],
+  controllers: [EmployerController]
+})
+export class EmployerModule { }
---
+++ backend/src/employer/schema/employer.schema.ts
@@ -0,0 +1,29 @@
+import { Document, Types } from "mongoose";
+import { Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+
+
+
+export type EmployerDocument=Document&Employer
+
+
+@Schema({timestamps:true})
+export class Employer{
+    @Prop({required:true})
+    email: string
+
+    @Prop({ required: true })
+    companyName: string
+    
+    @Prop({ required: true })
+    password:string
+    
+    @Prop({ type: [{ type: Types.ObjectId, ref: "Job" }], default: [] })
+    jobPosts: Types.ObjectId[]
+    
+    @Prop({ required: true ,default:"employer"})
+    role:string
+   
+
+}
+
+export const EmployerSchema=SchemaFactory.createForClass(Employer)
